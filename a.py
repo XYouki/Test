43a99bf6421dad35454125029090c0f9eb952a30
XYouki <33146530+XYouki@users.noreply.github.com>
2017-12-06 22:25:34 +1100

    Update a.py

--- a.py
+++ a.py
@@ -4,3 +4,24 @@ class Something:
 
 s = Something()
 s.out()
+
+class Something:
+    def out(self):
+        print("it works")
+
+s = Something()
+s.out()
+
+class Something:
+    def out(self):
+        print("it works")
+
+s = Something()
+s.out()
+
+class Something:
+    def out(self):
+        print("it works")
+
+s = Something()
+s.out()
